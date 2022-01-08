b5bea11efb0ee18949e2a5ebeb99fd9f86f9ae7e
Randy King Jr <rkingjr@outlook.com>
2022-01-08 15:36:00 -0600

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// Create start button in index.html
+// Style it in style.css
+// Event listener in Js
+// Start time in js
+// Create questions and answers in js and populate html dynamically
+// Questions need to include whether correct or not
+// Create event listener for each button
+// Go to first question
+
+
+// WHEN I answer a question
+// THEN I am presented with another question
+// Create ordered list within html
+// Within list
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+
+// WHEN the game is over
+// THEN I can save my initials and my score
